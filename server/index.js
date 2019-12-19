dfcde1995896a6e32560ee91c1c39f58bde3de76
kheabat25 <kheabat@gmail.com>
2019-12-19 11:00:18 +0800

    Fixed error

--- server/index.js
+++ server/index.js
@@ -61,7 +61,19 @@ io.on('connect', socket => {
     }
   });
 });
+/*
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
+});*/
 
 server.listen(process.env.PORT || 5000, () =>
-  console.log(`Server has started.`)
+  console.log(`Server has started`)
 );
